d21bcf79f2a54f1a9e936e427d95ab7325e33846
Hamad Alaslani <hamadsmma@gmail.com>
2025-06-23 09:53:26 +0300

    Add read toggle and delete book features

--- script.js
+++ script.js
@@ -12,6 +12,10 @@ function Book(title, author, pages, isRead) {
   this.id = crypto.randomUUID();
 }
 
+Book.prototype.toggleRead = function () {
+  this.isRead = !this.isRead;
+};
+
 function addBookToLibrary(title, author, pages, isRead) {
   const book = new Book(title, author, pages, isRead);
   bookLibrary.push(book);
@@ -23,7 +27,6 @@ function displayBook() {
   bookLibrary.forEach((book) => {
     const card = document.createElement("div");
     card.setAttribute("class", "card");
-
     const title = document.createElement("h3");
     title.textContent = book.title;
     const author = document.createElement("p");
@@ -34,9 +37,25 @@ function displayBook() {
     const bookButtons = document.createElement("div");
     bookButtons.setAttribute("class", "book-buttons");
     const readButton = document.createElement("button");
+    readButton.setAttribute("data-id", book.id);
     readButton.textContent = "Toggle Read";
     const delButton = document.createElement("button");
+    delButton.setAttribute("data-id", book.id);
     delButton.textContent = "Delete Book";
+
+    readButton.addEventListener("click", (e) => {
+      const id = e.target.getAttribute("data-id");
+      const editedBook = bookLibrary.find((book) => book.id === id);
+      editedBook.toggleRead();
+      displayBook();
+    });
+
+    delButton.addEventListener("click", (e) => {
+      const id = e.target.getAttribute("data-id");
+      const index = bookLibrary.findIndex((book) => book.id === id);
+      bookLibrary.splice(index, 1);
+      displayBook();
+    });
 
     card.append(title, author, isRead, bookButtons);
     bookButtons.append(readButton, delButton);
